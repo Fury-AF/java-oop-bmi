993324c3031d0bb3fb85c0eebc0089989e9b382f
Valdoveste <nickyv.oliveira@gmail.com>
2020-07-28 21:35:35 -0300

    the function "interpretationBMI()" has name changed to "diagnostic()" and received "DRY" in the attribution of variable "Result", function "calculteBMI()" now return a double.

--- src/BMI.java
+++ src/BMI.java
@@ -5,34 +5,35 @@ public class BMI {
         this.weight = weight;
     }
 
-    private String interpretationIMC(double bmiRAW) {
+    private String diagnostic(){
+        double bmiRAW = calculateBMI();
         String Result = "";
         String strBMI = String.format("%.02f", bmiRAW);
 
-        if (bmiRAW == 16) {
-            Result = "Very severe unweight" + "BMI: " + strBMI+"kg/m²";
+        if (bmiRAW <= 16) {
+            Result = "Very severe unweight";
         } else if (bmiRAW >= 16 && bmiRAW <= 16.99) {
-            Result = "Severe low weight" + "BMI: " + strBMI+"kg/m²";
+            Result = "Severe low weight";
         } else if (bmiRAW >= 17 && bmiRAW <= 18.49) {
-            Result = "Low weight" + "BMI: " + strBMI+"kg/m²";
+            Result = "Low weight";
         } else if (bmiRAW >= 18.50 && bmiRAW <= 24.99) {
-            Result = "Normal weight " + "BMI: " + strBMI+"kg/m²";
+            Result = "Normal weight";
         } else if (bmiRAW >= 25 && bmiRAW <= 29.99) {
-            Result = "Overweight " + "BMI: " + strBMI+"kg/m²";
+            Result = "Overweight";
         } else if (bmiRAW >= 30 && bmiRAW <= 34.99) {
-            Result = "Grade I obesity " + "BMI: " + strBMI+"kg/m²";
+            Result = "Grade I obesity";
         } else if (bmiRAW >= 35 && bmiRAW <= 39.99) {
-            Result = "Grade II obesity " + "BMI: " + strBMI+"kg/m²";
+            Result = "Grade II obesity";
         } else if (bmiRAW >= 40) {
-            Result = "Grade III obesity(morbid obesity) " + "BMI: " + strBMI+"kg/m²";
+            Result = "Grade III obesity(morbid obesity)";
         }
 
+        Result += " BMI: "+strBMI+"kg/m²";
         return Result;
     }
 
-    public String calculateBMI(){
-        double bmiRAW = weight / (height*height);
-        return interpretationIMC(bmiRAW);
+    public double calculateBMI(){
+       return weight / (height*height);
     }
 
 }
