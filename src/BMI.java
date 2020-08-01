e12dc5a4a4451b7b8cc5de6fe4e0929c4d6df313
Valdoveste <nickyv.oliveira@gmail.com>
2020-08-01 18:10:07 -0300

    following the activity's rules, and inserting more "DRY".

--- src/BMI.java
+++ src/BMI.java
@@ -1,31 +1,29 @@
-public class BMI {
-    private double height, weight;
-    public BMI(double weight, double height) {
-        this.height = height;
-        this.weight = weight;
+public class BMI  extends Athlete{
+    public BMI(String name, double weight, double height) {
+        super(name, height, weight);
     }
 
-    private String diagnostic(){
+    public String diagnostic(){
         double bmiRAW = calculateBMI();
-        String Result = "";
+        String Result = name+" has ";
         String strBMI = String.format("%.02f", bmiRAW);
 
         if (bmiRAW <= 16) {
-            Result = "Very severe unweight";
+            Result += "Very severe unweight";
         } else if (bmiRAW >= 16 && bmiRAW <= 16.99) {
-            Result = "Severe low weight";
+            Result += "Severe low weight";
         } else if (bmiRAW >= 17 && bmiRAW <= 18.49) {
-            Result = "Low weight";
+            Result += "Low weight";
         } else if (bmiRAW >= 18.50 && bmiRAW <= 24.99) {
-            Result = "Normal weight";
+            Result += "Normal weight";
         } else if (bmiRAW >= 25 && bmiRAW <= 29.99) {
-            Result = "Overweight";
+            Result += "Overweight";
         } else if (bmiRAW >= 30 && bmiRAW <= 34.99) {
-            Result = "Grade I obesity";
+            Result += "Grade I obesity";
         } else if (bmiRAW >= 35 && bmiRAW <= 39.99) {
-            Result = "Grade II obesity";
+            Result += "Grade II obesity";
         } else if (bmiRAW >= 40) {
-            Result = "Grade III obesity(morbid obesity)";
+            Result += "Grade III obesity(morbid obesity)";
         }
 
         Result += " BMI: "+strBMI+"kg/m²";
